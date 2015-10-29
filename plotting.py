be35c31a13fd954f0f7934733367746a76c3ecc0
Elias <ripagood@gmail.com>
2015-10-28 23:29:42 -0600

    DataVisualization

--- plotting.py
+++ plotting.py
@@ -2,8 +2,17 @@
 
 import sys
 
+#Run a test with cat log1Mote5.txt | awk 'BEGIN {FS=","}{print $10; fflush()}' | ./plotting.py | ./driveGnuPlots.pl 3 50 50 50 "X" "Z" "Y"
+
+
+
+#from the HMC5883 datasheet, for gain mode 5
+
+HMC_SCALE = 2.56
+
+ignore =0
 moteId=[1,2,3,4,5]
-hexList =[1,2]
+hexList =[]
 X_values=[]
 Y_values=[]
 Z_values=[]
@@ -13,11 +22,8 @@ def paddedHex ( number ):
 
 
 def toHex( asciiList ):    
-    for i in range(0,len(asciiList)-1):
-        #hexList.append(hex(int(asciiList[i]))+hex(int(asciiList[i+1]))[2:])
-        #print asciiList[i]+asciiList[i+1]
+    for i in range(0,len(asciiList),2):
 	hexList.append(paddedHex(asciiList[i])+paddedHex(asciiList[i+1])[2:])
-        i+=2
 	
     return
 
@@ -27,26 +33,46 @@ def toInt ( hList ):
     return
 
 def arrangeValues( numList):
-    for i in range(0,len(numList)):
+    for i in range(0,len(numList),3):
         X_values.append(numList[i])
 	Y_values.append(numList[i+1])
 	Z_values.append(numList[i+2])
-        i+=3
-return
+    return
+
+def scaleValues (values):
+    for i in range(0,len(values)):
+	values[i]=values[i]*HMC_SCALE
+    return
 
 
+         
 
 
 for line in sys.stdin:
-    data = line.split('-')
     
-    toHex(data)
-    toInt(hexList)
-    #print hexList
-    #print int(str(hexList[0]),16)
-    #print hexList[0]
-    arrangeValues(hexList)
-    hexList=[]
+    if ignore == 0:#ignore first input
+       ignore = ignore +1
+    else:
+       data = line.split('-')
+       toHex(data)
+       toInt(hexList)
+       arrangeValues(hexList)
+       scaleValues(X_values)
+       scaleValues(Y_values)
+       scaleValues(Z_values)
+       for i in range(0,len(X_values)):
+	   print "0:"+str(X_values[i])
+           sys.stdout.flush()
+           print "1:"+str(Y_values[i])
+           sys.stdout.flush()
+           print "2:"+str(Z_values[i])
+           sys.stdout.flush()
+
+     
+       X_values=[]
+       Y_values=[]
+       Z_values=[]
+       hexList=[]
     
      
     
